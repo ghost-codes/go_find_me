034ba56bb8d9b798381aeadf2eba0f2fad52efa2
hopedorkenoo <compounddork@gmail.com>
2021-07-03 14:56:05 +0000

    Profile Skeleton

--- lib/ui/profile_view.dart
+++ lib/ui/profile_view.dart
@@ -5,10 +5,37 @@ class ProfileView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Center(
-        child: Text("Profile"),
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.all(15),
+        color: Colors.white,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Center(
+                child: CircleAvatar(
+                  radius: 60,
+                  backgroundColor: Colors.grey,
+                ),
+              ),
+              SizedBox(height: 15),
+              Center(
+                child: Text(
+                  "Profile",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+              ),
+              Column(
+                  children: List.generate(
+                      10,
+                      (index) => Container(
+                            margin: EdgeInsets.only(top: 10),
+                            height: 35,
+                            color: Colors.grey[300],
+                          )))
+            ],
+          ),
+        ),
       ),
     );
   }
