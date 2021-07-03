d47c3080601f22521e34edbdddbba89283ec5cca
hopedorkenoo <compounddork@gmail.com>
2021-07-03 14:51:25 +0000

    Create Post Skeleton

--- lib/ui/create_post.dart
+++ lib/ui/create_post.dart
@@ -5,10 +5,45 @@ class CreatePostView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Center(
-        child: Text("Create Post"),
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Container(
+          color: Colors.white,
+          padding: EdgeInsets.all(15),
+          child: Column(
+            children: [
+              TextField(
+                maxLines: 20,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey,
+                        width: 1,
+                        style: BorderStyle.solid,
+                      ),
+                    ),
+                    hintText: "Create Post"),
+              ),
+              SizedBox(height: 15),
+              TextButton(
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, '/');
+                },
+                child: Container(
+                  decoration: BoxDecoration(color: Colors.black),
+                  width: double.infinity,
+                  padding: EdgeInsets.all(15),
+                  child: Center(
+                    child: Text(
+                      "Sign Up",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
