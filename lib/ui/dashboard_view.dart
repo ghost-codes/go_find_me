cdecbcf12ff7aa9d864b21898c646120559dd638
hopedorkenoo <compounddork@gmail.com>
2021-07-03 14:45:48 +0000

    Dashboard Skeleton

--- lib/ui/dashboard_view.dart
+++ lib/ui/dashboard_view.dart
@@ -6,10 +6,56 @@ class DashboardView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Center(
-        child: Text("DashBoard"),
+    return SafeArea(
+      child: Container(
+        color: Colors.white,
+        child: Column(
+          // crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Posts",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 30,
+                itemBuilder: (context, index) => Container(
+                  margin: EdgeInsets.only(top: 15, left: 15),
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(5),
+                      topLeft: Radius.circular(5),
+                    ),
+                  ),
+                  height: 200,
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          color: Colors.grey,
+                        ),
+                      ),
+                      SizedBox(height: 15),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Icon(Icons.thumb_up),
+                          Icon(Icons.share),
+                          Icon(Icons.chat),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
