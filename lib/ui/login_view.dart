e4ab4e8cff23e9ccd5b0e20af7e74e8afe4fe773
hopedorkenoo <compounddork@gmail.com>
2021-08-04 16:17:12 +0000

    Login View

--- lib/ui/login_view.dart
+++ lib/ui/login_view.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:project_android/components/text_fields.dart';
+import 'package:project_android/themes/borderRadius.dart';
+import 'package:project_android/themes/buttons.dart';
+import 'package:project_android/themes/dropShadows.dart';
+import 'package:project_android/themes/padding.dart';
+import 'package:project_android/themes/textStyle.dart';
+import 'package:project_android/themes/theme_colors.dart';
 
 class Login extends StatelessWidget {
   const Login({Key? key}) : super(key: key);
@@ -7,25 +13,64 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.all(15),
-
-        // height: double.infinity,
+      body: SafeArea(
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                height: 500,
-                child: Center(
-                  child: Container(
-                    color: Colors.black,
-                    width: 100,
-                    height: 100,
+          child: Container(
+            color: ThemeColors.white,
+            padding: EdgeInsets.all(2 * ThemePadding.padBase),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 30,
+                ),
+                CircleAvatar(
+                  backgroundColor: ThemeColors.primary,
+                  radius: 80,
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  height: 500,
+                  padding: EdgeInsets.all(2.5 * ThemePadding.padBase),
+                  decoration: BoxDecoration(
+                    color: ThemeColors.white,
+                    borderRadius: ThemeBorderRadius.bigRadiusAll,
+                    boxShadow: ThemeDropShadow.bigShadow,
+                  ),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: ThemePadding.padBase * 3,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Login",
+                            style: ThemeTexTStyle.headerPrim,
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.pushReplacementNamed(
+                                  context, '/signup');
+                            },
+                            child: Text(
+                              "Sign Up",
+                              style: ThemeTexTStyle.regular,
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(height: 2.5 * ThemePadding.padBase),
+                      LoginForm(),
+                    ],
                   ),
                 ),
-              ),
-              LoginForm()
-            ],
+              ],
+            ),
           ),
         ),
       ),
@@ -42,44 +87,35 @@ class LoginForm extends InputDec {
       child: Column(
         children: [
           TextFormField(
-            decoration: inputDec(label: "Email"),
+            decoration: inputDec(hint: "Username or Email"),
           ),
+          SizedBox(height: 2 * ThemePadding.padBase),
           TextFormField(
-            decoration: inputDec(label: "Password"),
+            decoration: inputDec(hint: "Password"),
             obscureText: true,
           ),
           SizedBox(height: 50),
-          TextButton(
-            onPressed: () {
+          ThemeButton.longButtonPrim(
+            text: "Login",
+            onpressed: () {
               Navigator.pushReplacementNamed(context, '/');
             },
-            child: Container(
-              decoration: BoxDecoration(color: Colors.black),
-              width: double.infinity,
-              padding: EdgeInsets.all(15),
-              child: Center(
-                child: Text(
-                  "Login",
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-            ),
           ),
-          SizedBox(height: 20),
-          Row(
-            children: [
-              Text("Don't have an account? "),
-              GestureDetector(
-                onTap: () {
-                  Navigator.pushReplacementNamed(context, '/signup');
-                },
-                child: Text(
-                  "Sign Up",
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-              )
-            ],
-          )
+          Divider(
+            height: ThemePadding.padBase * 3,
+          ),
+          Text(
+            "or",
+            style: ThemeTexTStyle.regular,
+          ),
+          SizedBox(
+            height: ThemePadding.padBase,
+          ),
+          ThemeButton.longButtonSec(
+              text: "Google",
+              onpressed: () {
+                Navigator.pushReplacementNamed(context, '/');
+              })
         ],
       ),
     );
